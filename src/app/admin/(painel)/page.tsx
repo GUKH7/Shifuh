055caf856c6c3e643c44d8abf20382dfd36ad99c
GUKH7 <keiohirakawa@gmail.com>
2026-07-27 23:33:23 -0300

    Remover aprimorador de SVG do Dashboard

--- src/app/admin/(painel)/page.tsx
+++ src/app/admin/(painel)/page.tsx
@@ -1,12 +1,10 @@
 import DashboardPeriodWorkspace from "./DashboardPeriodWorkspace";
-import DashboardTopProductsEnhancer from "./DashboardTopProductsEnhancer";
 import styles from "./dashboard-period.module.css";
 
 export default function AdminHomePage() {
   return (
     <div className={styles.page}>
       <DashboardPeriodWorkspace />
-      <DashboardTopProductsEnhancer />
     </div>
   );
 }
