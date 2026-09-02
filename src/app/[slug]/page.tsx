c01fa06aa748d2f2d2e06c8df98683520cc7c3f9
GUKH7 <keiohirakawa@gmail.com>
2026-09-02 08:40:17 -0300

    refactor: keep storefront route thin with lucky wheel bridge

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,13 +1,4 @@
 "use client";
-
 import StorefrontPage from "@/features/storefront/StorefrontPage";
-import LuckyWheelStorefrontBridge from "@/features/storefront/LuckyWheelStorefrontBridge";
-
-export default function StorefrontRoutePage() {
-  return (
-    <>
-      <StorefrontPage />
-      <LuckyWheelStorefrontBridge />
-    </>
-  );
-}
+import Wheel from "@/features/storefront/LuckyWheelStorefrontBridge";
+export default function Page(){return <><StorefrontPage/><Wheel/></>}
