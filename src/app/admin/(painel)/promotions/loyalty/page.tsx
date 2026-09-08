f8934e194a6594ed2733ffc6d853226d7d5d9e9e
GUKH7 <keiohirakawa@gmail.com>
2026-09-08 10:05:02 -0300

    feat: conectar catálogo à página de fidelidade

--- src/app/admin/(painel)/promotions/loyalty/page.tsx
+++ src/app/admin/(painel)/promotions/loyalty/page.tsx
@@ -1,10 +1,12 @@
 import LoyaltyProgramWorkspace from "./LoyaltyProgramWorkspace";
+import LoyaltyRewardCatalog from "./LoyaltyRewardCatalog";
 import LoyaltyWalletLedger from "./LoyaltyWalletLedger";
 
 export default function PromotionsLoyaltyPage() {
   return (
     <>
       <LoyaltyProgramWorkspace />
+      <LoyaltyRewardCatalog />
       <LoyaltyWalletLedger />
     </>
   );
