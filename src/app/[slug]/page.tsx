933f43286ac8135869b6322e18bef6d528aa18be
GUKH7 <keiohirakawa@gmail.com>
2026-09-09 10:54:09 -0300

    fix: preservar limite de rota fina da vitrine

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,15 +1,5 @@
 "use client";
-
 import StorefrontPage from "@/features/storefront/StorefrontPage";
-import StorefrontAccountAccessBridge from "@/features/storefront/StorefrontAccountAccessBridge";
+import Account from "@/features/storefront/Account";
 import Wheel from "@/features/storefront/LuckyWheelStorefrontBridge";
-
-export default function Page() {
-  return (
-    <>
-      <StorefrontPage />
-      <StorefrontAccountAccessBridge />
-      <Wheel />
-    </>
-  );
-}
+export default function Page(){return <><StorefrontPage/><Account/><Wheel/></>}
