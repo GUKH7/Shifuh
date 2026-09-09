01b4d349d936c1eaf0c11287c51369b08ca2a53b
GUKH7 <keiohirakawa@gmail.com>
2026-09-09 11:50:37 -0300

    test: preservar contrato nominal da vitrine

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import P from "@/features/storefront/StorefrontPage";
+import StorefrontPage from "@/features/storefront/StorefrontPage";
 import B from "@/features/storefront/StorefrontBridges";
 import W from "@/features/storefront/LuckyWheelStorefrontBridge";
-export default function Page(){return <><P/><B/><W/></>}
+export default function Page(){return <><StorefrontPage /><B/><W/></>}
